57467b85fda1d48e559bfdd08867085db10d99e2
nyx500 <45918281+nyx500@users.noreply.github.com>
2022-08-23 18:00:37 +0100

    Added social media bar

--- Scripts/script.js
+++ Scripts/script.js
@@ -4,7 +4,7 @@ var myWhite = 'var(--off-white)';
 var myRed = 'var(--this-red)';
 var myLightGrey = 'var(--this-light-grey)';
 var linearGradientLight = 'linear-gradient(var(--this-mid-grey), var(--off-black), var(--this-mid-grey))';
-var linearGradientDark = 'linear-gradient(var(--this-light-grey), var(--off-white), var(--this-light-grey))';
+var linearGradientDark = 'linear-gradient(var(--this-mid-grey), var(--off-white), var(--this-mid-grey))';
 // FREQUENTLY USED (FOR DARK/LIGHT MODE) HTML ELEMENTS THAT MUST CHANGE COLOR
 // Links and buttons on the left of the navbar
 var leftHandMenuLinks = [
@@ -41,18 +41,28 @@ var footerLinks = [
 // SETS COLOR OF HTML ELEMENTS WHEN NOT IN "DARK MODE"
 function lightColorSettings()
 {
+  var button = document.getElementById('dark-mode');
+  button.innerHTML = "Dark Mode";
   var stickyNav = document.getElementsByClassName('sticky1')[0];
 
   document.getElementsByTagName('main')[0].style.backgroundColor = myWhite;
   document.getElementsByTagName('main')[0].style.color = myBlack;
   document.getElementsByTagName('footer')[0].style.backgroundColor = myLightGrey;
   document.getElementsByTagName('footer')[0].style.color = myBlack;
+
   if (document.getElementsByClassName('load-more')[0])
   { 
     var loadMoreButton = document.getElementsByClassName('load-more')[0];
     loadMoreButton.style.backgroundImage = linearGradientLight;
     loadMoreButton.style.color = myWhite;
     loadMoreButton.style.border = '2px solid var(--off-black)';
+    // When mouse goes over the Load More... buttons, make a box-shadow in grey
+    loadMoreButton.addEventListener('mouseover', function(e) {
+      e.target.style.boxShadow = '5px 5px var(--this-light-grey)';
+    })
+    loadMoreButton.addEventListener('mouseout', function(e) {
+      e.target.style.boxShadow = 'none';
+    }) 
   }
 
   if (document.getElementById('aside-reviews'))
@@ -104,6 +114,9 @@ function lightColorSettings()
 
 // SETS COLOR OF HTML ELEMENTS IN "DARK MODE"
 function darkColorSettings() {
+  var button = document.getElementById('dark-mode');
+  button.innerHTML = "Light Mode";
+
   var stickyNav = document.getElementsByClassName('sticky1')[0];
   document.getElementsByTagName('main')[0].style.backgroundColor = myBlack;
   document.getElementsByTagName('main')[0].style.color = myWhite;
@@ -116,7 +129,12 @@ function darkColorSettings() {
     var loadMoreButton = document.getElementsByClassName('load-more')[0];
       loadMoreButton.style.backgroundImage = linearGradientDark;
       loadMoreButton.style.color = myBlack;
-      loadMoreButton.style.border = '2px solid var(--this-red)';
+    loadMoreButton.addEventListener('mouseover', function(e) {
+      e.target.style.boxShadow = '5px 5px var(--this-mid-grey)';
+    })
+    loadMoreButton.addEventListener('mouseout', function(e) {
+      e.target.style.boxShadow = 'none';
+    })
   }
 
   if (document.getElementById('aside-reviews'))
@@ -167,17 +185,14 @@ function darkColorSettings() {
 
 // SETS LOCAL STORAGE FOR DARK AND LIGHT MODE
 function changeColorMode(){
-  var darkLightButton = document.getElementById('dark-mode');
   if (localStorage.getItem('colorMode') == 'light')
   {
     darkColorSettings();
-    darkLightButton.innerHTML = 'Light Mode';
     localStorage.setItem('colorMode', 'dark');
   }
   else
   {
     lightColorSettings();
-    darkLightButton.innerHTML = 'Dark Mode';
     localStorage.setItem('colorMode', 'light');
   }
 }
@@ -324,6 +339,19 @@ function addShowExtraItemsListeners(){
     )
   }
 
+  // Music page load more button
+  if (document.getElementById('load-more-music'))
+  { 
+    createEventListenerForShowMoreButton(
+      document.getElementById('load-more-music'),
+      'bottom_music_elements',
+      document.getElementById('review-articles-music'),
+      document.getElementsByClassName('reviews-title')[0],
+      3,
+      'music-not-hidden'
+    )
+  }
+
   // Travel page load more button
   if (document.getElementById('load-more-travel'))
   { 
@@ -418,15 +446,35 @@ function changeTextSize(pixels) {
     if (window.innerWidth > 768)
     {
       changeMainHeadingSize('5.2rem');
+      // Change size of images on Book/Film review pages
+      // Avoids overflow over pages
+      changeReviewIconSize('12rem');
     }
-    // Change main heading size for small screens
     else
     {
-      changeMainHeadingSize('12vw');
+      changeMainHeadingSize('2rem');
+      changeReviewIconSize('10rem');
+      if(document.getElementsByClassName('reviews-expand')[0])
+      {
+        var bottomArea = document.getElementsByClassName('reviews-expand')[0];
+        bottomArea.style.gridTemplateColumns = '1fr';
+        bottomArea.style.gridTemplateRows = '1fr 1fr';
+        bottomArea.style.gridTemplateAreas = ' "loadbutton" "uparrow"';
+        bottomArea.style.justifyContent = 'start';
+        bottomArea.style.justifyItems = 'start';
+      }
+      else if (document.getElementsByClassName('travel-events-expand')[0])
+      {
+        console.log('x');
+        var bottomArea = document.getElementsByClassName('travel-events-expand')[0];
+        bottomArea.style.gridTemplateColumns = '1fr';
+        bottomArea.style.gridTemplateRows = '1fr 1fr';
+        bottomArea.style.gridTemplateAreas = ' "loadbutton" "uparrow"';
+        bottomArea.style.justifyContent = 'start';
+        bottomArea.style.justifyItems = 'start';
+      }
     }
-    // Change size of images on Book/Film review pages
-    // Avoids overflow over pages
-    changeReviewIconSize('11rem');
+
   }
   // MEDIUM TEXT SIZE
   else if (pixels == '18px')
@@ -483,8 +531,15 @@ function textSizeListeners() {
     if (text_choice_buttons[i].id == "large-text")
       { 
         text_choice_buttons[i].addEventListener('click', function()
-        {
-          changeTextSize('22px');
+        { 
+          if (window.innerWidth > 768)
+          { 
+            changeTextSize('22px');
+          }
+          else
+          {
+            changeTextSize('20px');
+          }
         })
       }
       else if (text_choice_buttons[i].id == "medium-text")
@@ -513,9 +568,9 @@ function textSizeListeners() {
 
 //SELECT FORM EVENT LISTENERS
 function selectFormEventListeners()
-{
+{ 
   if  (document.getElementById('subscribe-form'))
-  {
+  { 
     var form = document.getElementById('subscribe-form');
     var formTextInputs = document.getElementsByClassName('form-text-input');
     formTextInputs = Array.from(formTextInputs);
@@ -550,15 +605,75 @@ function selectFormEventListeners()
     })
   })
 
+  // Stores form submission button
   var submitButton = document.getElementById('form-submit');
+  // When button is clicked, decide whether form is valid
   submitButton.addEventListener('click', (e) => {
-    // Prevents automatic reload of page
+    // Counter keeps track of how many input errors the user has made
+    var formInputErrors = 0;
+
+    // Deletes all the error messages from previous submit
+    if (document.getElementsByClassName('form-error'))
+    {
+      var errors = document.getElementsByClassName('form-error');
+      errors = Array.from(errors);
+      // Attribution: https://www.javascripttutorial.net/javascript-dom/javascript-removechild/
+      errors.forEach(err => {
+        form.removeChild(err);
+      })
+    }
+
+    // Prevents automatic reload of page onclick of submit button
+    // This is because we have no back-end --> form data is not actually being sent anywhere...
     e.preventDefault();
 
-    var formTitle = document.getElementById('subscribe-form-heading');
-    formTitle.style.display = 'none';
-    form.innerHTML = '<div id="thank-you-message"><h4>Thank you for signing up!</h4></div>';
-    scrollToJustAboveElement (document.getElementById('thank-you-message'), 400);
+    for (var i = 0; i < formTextInputs.length; i++)
+    { 
+      // For all the obligatory (text) inputs in the form, reset border to black
+      formTextInputs[i].style.border = '2px solid var(--off-black)';
+      // Checks for empty inputs
+      // Attribution: https://www.w3resource.com/javascript/form/non-empty-field.php
+      if (formTextInputs[i].value.length === 0)
+      { 
+        // Inserts an error message above the empty input
+        var error = document.createElement("div");
+        error.innerHTML = 'Please fill in this field!';
+        error.className = 'form-error';
+        error.id = `error${i + 1}`;
+        formTextInputs[i].style.border = '2px solid var(--this-dark-red)';
+        // Attribution: https://attacomsian.com/blog/javascript-dom-get-the-parent-of-an-element#:~:text=To%20get%20the%20parent%20node,element%20as%20a%20Node%20object.&text=The%20parentNode%20property%20is%20read,you%20can%20not%20modify%20it.
+        form.insertBefore(error, formTextInputs[i].parentNode);
+        // Increments the number of errors
+        formInputErrors++;
+      }
+    }
+
+    // Checks for invalid email address --> no '@' symbol in the email
+    // Attribution: https://www.w3schools.com/jsref/jsref_includes.asp
+    if (document.getElementById('email').value.length > 0 && !document.getElementById('email').value.includes('@'))
+    { 
+      var error = document.createElement("div");
+      error.innerHTML = 'Invalid email address.';
+      error.className = 'form-error';
+      error.id = `error${4}`;
+      document.getElementById('email').style.border = '2px solid var(--this-dark-red)';
+      form.insertBefore(error, document.getElementById('email').parentNode);
+      // Increments the number of errors
+      formInputErrors++;
+    }
+
+    // If form is valid, do this
+    if (formInputErrors == 0)
+    { 
+      // Hide form title and form
+      var formTitle = document.getElementById('subscribe-form-heading');
+      formTitle.style.display = 'none';
+      formTitle.style.display = 'none';
+      // Replace form with a thank you message and link back to homepage
+      form.innerHTML = '<div id="thank-you-message"><h4>Thank you for signing up!</h4> <br> <h5 id="back-link">Back to <a href="index.html">Home</a></h5></div>';
+      // Scroll to beginning of thank you message
+      scrollToJustAboveElement (document.getElementById('thank-you-message'), 400);
+    }
   })
 }
 }
@@ -605,6 +720,13 @@ window.onload = function(){
     localStorage.setItem('bottom_films_elements', 'hidden');
   }
 
+  if (document.getElementById('review-articles-music'))
+  {
+    repeatedElementsContainer = document.getElementById('review-articles-music');
+    hideItems(repeatedElementsContainer, 3, 'music-not-hidden');
+    localStorage.setItem('bottom_music_elements', 'hidden');
+  }
+
   if (document.getElementById('travel-content'))
   { 
     repeatedElementsContainer = document.getElementById('travel-content');
@@ -630,6 +752,23 @@ window.onload = function(){
   textSizeListeners();
   selectFormEventListeners();  
 }
+
+// Identifies if page is fully loaded
+// If not fully loaded, display the loader icon
+// Attribution: https://www.jstips.co/en/javascript/detect-document-ready-in-pure-js/
+document.onreadystatechange = () => {
+  // Gets the loader icon by class name
+  var loader = document.getElementsByClassName('loader')[0];
+  // If page finished loading, hide the loader
+  if (document.readyState === 'complete') {
+    loader.style.display = 'none';
+  }
+  // If page still loading, display the loader
+  else
+  {
+    loader.style.display = 'block';
+  }
+};
 
 // Closes dropdown menu on outside click
 document.addEventListener("click", function(event){
@@ -670,7 +809,7 @@ function makeSticky() {
     // How to make an array from an HTMLCollection, so that the ForEach() method
     // becomes available for iterating through the collection
     textSizeOptions = Array.from(textSizeOptions);
-    var navLinks = document.getElementById('main-navbar-list').children;
+    var navLinks = document.getElementsByClassName('main-nav-link');
     navLinks = Array.from(navLinks);
         
     if (window.pageYOffset > nav.offsetTop) 
@@ -707,21 +846,21 @@ function makeSticky() {
       navLinks.forEach(link => {
         if (localStorage.getItem('colorMode') == 'light')
         { 
-          link.children[0].style.color = myBlack;
+          link.style.color = myBlack;
           
-          link.children[0].addEventListener('mouseout', () => {
-          link.children[0].style.color = myBlack;
+          link.addEventListener('mouseout', () => {
+          link.style.color = myBlack;
           });
         }
         else
         {
-          link.children[0].style.color = myWhite;
-          link.children[0].addEventListener('mouseout', () => {
-            link.children[0].style.color = myWhite;
+          link.style.color = myWhite;
+          link.addEventListener('mouseout', () => {
+            link.style.color = myWhite;
             });
         }
-        link.children[0].addEventListener('mouseover', () => {
-          link.children[0].style.color = myRed;
+        link.addEventListener('mouseover', () => {
+          link.style.color = myRed;
         });
     })
   }
@@ -734,12 +873,12 @@ function makeSticky() {
       })
       
       navLinks.forEach(link => {
-        link.children[0].style.color = myWhite;
-        link.children[0].addEventListener('mouseover', () => {
-          link.children[0].style.color = myRed;
+        link.style.color = myWhite;
+        link.addEventListener('mouseover', () => {
+          link.style.color = myRed;
         });
-        link.children[0].addEventListener('mouseout', () => {
-          link.children[0].style.color = myWhite;
+        link.addEventListener('mouseout', () => {
+          link.style.color = myWhite;
         })
       })
       document.getElementById('navbar-logo-text').style.color = myWhite;
